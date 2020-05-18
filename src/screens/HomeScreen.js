9c98f6b11bef843c85cf4c47a950d89b79236a79
Mike Cossalter <mikecossalter@yahoo.com>
2020-05-18 17:37:20 -0500

    added a ImageScreen and created a button on the HomeScreen to direct us to it

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({navigation}) => {
   return (
     <View>
       <Text style={styles.text}>Hi There!</Text>
       <Button
-        onPress={() => props.navigation.navigate('Components')}
+        onPress={() => navigation.navigate('Components')}
         title="Go to Components Demo"
       />
       <Button 
-      onPress={() => props.navigation.navigate('List')}
+      onPress={() => navigation.navigate('List')}
       title="Go to List Demo">
-        <Text>Go to List Demo</Text>
      </Button>
+      <Button
+        onPress={() => navigation.navigate('ImageScreen')}
+        title="Go to ImageScreen">
+      </Button>
     </View>
   );
 };
