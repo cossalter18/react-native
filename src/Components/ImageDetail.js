03db49bc80893a7d4b2a92379cd3ec8a43f17d38
Mike Cossalter <mikecossalter@yahoo.com>
2020-05-18 19:11:13 -0500

    passing image props and destructuring

--- src/Components/ImageDetail.js
+++ src/Components/ImageDetail.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 
-const ImageDetail = (props) => {
- 
-    
-    return <Text>{props.title}</Text>
+const ImageDetail = ({imageSource, title, score}) => {
+   
+    return <View>
+        <Image source= {imageSource} />
+        <Text>{title}</Text>
+        <Text>Image Score - {score}</Text>
+        </View>
 }
 
 const styles = StyleSheet.create({});
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 //import component to show several times
 import ImageDetail from '../Components/ImageDetail'
 
 
-const ImageScreen = () =>{
+const ImageScreen = () => {
     return <View>
         {/* we can choose any prop name that we want, we used "title" */}
-        <ImageDetail title="Forest"/>
-        <ImageDetail title="Beach"/>
-        <ImageDetail title="Mountain"/>
+        {/* we added another prop that WE NAMED, imageSource*/}
+        <ImageDetail title="Forest" score={9} imageSource={require('../../assets/forest.jpg')} />
+        <ImageDetail title="Beach" score={7} imageSource={require('../../assets/beach.jpg')} />
+        <ImageDetail title="Mountain" score={10} imageSource={require('../../assets/mountain.jpg')} />
     </View>
 }
 
